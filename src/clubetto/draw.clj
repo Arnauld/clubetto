0a44f3a3c21d1617442615e03dff3f0cb27849e0
Arnauld <arnauld.loyer@gmail.com>
2017-06-29 17:16:30 +0200

    draw axis

--- src/clubetto/draw.clj
+++ src/clubetto/draw.clj
@@ -3,9 +3,7 @@
             [quil.middleware :as m]
             [clubetto.core :as c]))
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
+(defn draw-cubetto [state]
   (let [sz (:sz state)
         hsz (/ sz 2)]
     ; Move origin point to the center of the cubetto.
@@ -14,11 +12,29 @@
       (q/with-rotation
         [(:angle state)]
         ; Draw the cubetto
+        (q/stroke-weight 1)
+        (q/stroke 0)
         (q/fill 83 160 219)
         (q/rect (- hsz) (- hsz) sz sz)
         (q/fill 0)
         (q/ellipse -10 (- hsz 10) 7 7)
         (q/ellipse 10 (- hsz 10) 7 7)))))
+
+(defn draw-axis [state]
+  (let [sz (:sz state)]
+    (q/stroke-weight 2)
+    (q/stroke 255 0 0)
+    (q/line 10 10 sz 10)
+    (q/text "x" sz 10)
+    (q/stroke 0 255 0)
+    (q/line 10 10 10 sz)
+    (q/text "y" 10 sz)))
+
+(defn draw-state [state]
+  ; Clear the sketch by filling it with light-grey color.
+  (q/background 240)
+  (draw-axis state)
+  (draw-cubetto state))
 
 
 
